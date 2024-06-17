13703e4cf0f1df4ec1b8fd08770c32c6415d5cf0
Olivier Cailloux <olivier.cailloux@gmail.com>
2024-06-17 14:56:46 +0200

    test cp

--- src/test/java/io/github/oliviercailloux/grading/additioner/MyTests.java
+++ src/test/java/io/github/oliviercailloux/grading/additioner/MyTests.java
@@ -1,18 +1,30 @@
 package io.github.oliviercailloux.grading.additioner;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static com.google.common.base.Preconditions.checkState;
+import static io.github.oliviercailloux.jaris.exceptions.Unchecker.URI_UNCHECKER;
 
+import com.google.common.io.Resources;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+import java.nio.file.Path;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MyTests {
-	@SuppressWarnings("unused")
-	private static final Logger LOGGER = LoggerFactory.getLogger(MyTests.class);
+  @SuppressWarnings("unused")
+  private static final Logger LOGGER = LoggerFactory.getLogger(MyTests.class);
 
-	@Test
-	void testSomething() throws Exception {
-		LOGGER.info("Started tests.");
-		assertTrue(false);
-	}
+  @Test
+  void testSomething() throws Exception {
+    LOGGER.info("Started tests.");
+    InputStream inp = io.github.oliviercailloux.javagrade.bytecode.Compiler.class.getResourceAsStream("Eclipse-prefs.epf");
+    checkState(inp != null);
+    final URL propertiesUrl = io.github.oliviercailloux.javagrade.bytecode.Compiler.class.getResource("Eclipse-prefs.epf");
+    checkState(propertiesUrl != null);
+    URI uri = URI_UNCHECKER.getUsing(() -> propertiesUrl.toURI());
+    LOGGER.info("URI: {}.", uri);
+    final Path propertiesPath = Path.of(uri);
+  }
 }
