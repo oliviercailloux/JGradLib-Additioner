a7ed0dd3aae680b51bbaca8dae761bf173f2dae3
Olivier Cailloux <olivier.cailloux@gmail.com>
2024-06-26 00:17:39 +0200

    Test illustrates JUnit bug

--- src/test/java/io/github/oliviercailloux/grading/additioner/MyTests.java
+++ src/test/java/io/github/oliviercailloux/grading/additioner/MyTests.java
@@ -1,14 +1,41 @@
 package io.github.oliviercailloux.grading.additioner;
 
 import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.base.Verify.verify;
 import static io.github.oliviercailloux.jaris.exceptions.Unchecker.URI_UNCHECKER;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
+import com.google.common.io.ByteSource;
+import com.google.common.io.MoreFiles;
 import com.google.common.io.Resources;
-import java.io.InputStream;
+import io.github.oliviercailloux.javagrade.JUnitHelper;
+import io.github.oliviercailloux.javagrade.bytecode.Compiler;
+import io.github.oliviercailloux.javagrade.graders.AdditionerTests;
+import io.github.oliviercailloux.utils.Utils;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.net.URI;
 import java.net.URL;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Optional;
+import java.util.Set;
+import org.eclipse.jdt.core.compiler.batch.BatchCompiler;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.engine.discovery.DiscoverySelectors;
+import org.junit.platform.launcher.Launcher;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.LauncherSession;
+import org.junit.platform.launcher.TestIdentifier;
+import org.junit.platform.launcher.TestPlan;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+import org.junit.platform.launcher.listeners.discovery.LauncherDiscoveryListeners;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,14 +44,25 @@ public class MyTests {
   private static final Logger LOGGER = LoggerFactory.getLogger(MyTests.class);
 
   @Test
+  void testSomethingD() throws Exception {
+    Compiler.eclipseCompile(ImmutableList.of(), ImmutableSet.of(), true, Optional.empty());
+    String packageName = AdditionerTests.class.getPackageName();
+    JUnitHelper.grade(packageName, null);
+    JUnitHelper.grade(packageName, null);
+    LOGGER.info("Graded.");
+  }
+
+  @Test
   void testSomething() throws Exception {
-    LOGGER.info("Started tests.");
-    InputStream inp = io.github.oliviercailloux.javagrade.bytecode.Compiler.class.getResourceAsStream("Eclipse-prefs.epf");
-    checkState(inp != null);
-    final URL propertiesUrl = io.github.oliviercailloux.javagrade.bytecode.Compiler.class.getResource("Eclipse-prefs.epf");
-    checkState(propertiesUrl != null);
-    URI uri = URI_UNCHECKER.getUsing(() -> propertiesUrl.toURI());
-    LOGGER.info("URI: {}.", uri);
-    final Path propertiesPath = Path.of(uri);
+    BatchCompiler.compile(new String[] {"--release", "17", "Something.java"}, new PrintWriter(new StringWriter()),
+        new PrintWriter(new StringWriter()), null);
+
+    String packageName = AdditionerTests.class.getPackageName();
+    LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
+        .selectors(DiscoverySelectors.selectPackage(packageName)).build();
+    try (LauncherSession session = LauncherFactory.openSession()) {
+      Launcher launcher = session.getLauncher();
+      launcher.discover(request);
+    }
   }
 }
